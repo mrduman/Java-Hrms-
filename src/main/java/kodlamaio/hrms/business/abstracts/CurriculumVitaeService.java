e695395fcb5e3e761029b1625ffa2949ce393b68
mrduman <omer.duman@gmx.de>
2021-10-01 18:06:29 +0200

    aaa

--- src/main/java/kodlamaio/hrms/business/abstracts/CurriculumVitaeService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/CurriculumVitaeService.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface CurriculumVitaeService {
 
     DataResult<List<CurriculumVitae>> getAll();
+    
     public DataResult<List<CurriculumVitae>> getByCandidate_Id(int id);
 
     Result add(CurriculumVitae curriculumVitae);
